dd09d1b29fac2f34dd8eb67f368dfb9c518d2b18
Oleksandra Fogel <113315714+oleksafogel@users.noreply.github.com>
2024-05-17 10:29:20 +0200

    test

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider, connect } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import App from './containers/App';
